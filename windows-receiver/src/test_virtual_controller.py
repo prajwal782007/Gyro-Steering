bd8e3fc3d5923a976d0a4a2b7919ed676d4f842a
Prajwal <prajwalvalekar08@gmail.com>
2026-07-15 23:10:53 +0530

    test: add unit tests for VirtualController steering and throttle mapping logic

--- windows-receiver/src/test_virtual_controller.py
+++ windows-receiver/src/test_virtual_controller.py
@@ -18,8 +18,10 @@ class TestVirtualController(unittest.TestCase):
         # We manually centered in __init__, but let's test the center() method
         self.controller.center()
         self.assertEqual(self.controller.current_normalized_x, 0.0)
+        self.assertEqual(self.controller.current_throttle, 0.0)
         self.assertTrue(self.controller.is_centered)
         self.controller.gamepad.left_joystick_float.assert_called_with(x_value_float=0.0, y_value_float=0.0)
+        self.controller.gamepad.right_trigger_float.assert_called_with(value_float=0.0)
 
     def test_proportional_mapping(self):
         test_cases = [
@@ -36,16 +38,39 @@ class TestVirtualController(unittest.TestCase):
 
         for angle, expected_normalized in test_cases:
             with self.subTest(angle=angle):
-                self.controller.set_steering_angle(angle)
+                self.controller.set_control_state(angle, 0.0)
                 self.assertAlmostEqual(self.controller.current_normalized_x, expected_normalized, places=5)
                 self.controller.gamepad.left_joystick_float.assert_called_with(x_value_float=self.controller.current_normalized_x, y_value_float=0.0)
 
     def test_clamping(self):
-        self.controller.set_steering_angle(-120.0)
+        self.controller.set_control_state(-120.0, 0.0)
         self.assertEqual(self.controller.current_normalized_x, -1.0)
         
-        self.controller.set_steering_angle(130.0)
+        self.controller.set_control_state(130.0, 0.0)
         self.assertEqual(self.controller.current_normalized_x, 1.0)
 
+    def test_throttle_proportional_mapping(self):
+        test_cases = [
+            (0.000, 0.000),
+            (0.250, 0.250),
+            (0.500, 0.500),
+            (0.750, 0.750),
+            (1.000, 1.000)
+        ]
+
+        for throttle, expected in test_cases:
+            with self.subTest(throttle=throttle):
+                self.controller.set_control_state(0.0, throttle)
+                self.assertAlmostEqual(self.controller.current_throttle, expected, places=5)
+                self.controller.gamepad.right_trigger_float.assert_called_with(value_float=self.controller.current_throttle)
+
+    def test_throttle_clamping(self):
+        self.controller.set_control_state(0.0, -0.5)
+        self.assertEqual(self.controller.current_throttle, 0.0)
+        
+        self.controller.set_control_state(0.0, 1.5)
+        self.assertEqual(self.controller.current_throttle, 1.0)
+
 if __name__ == '__main__':
+
     unittest.main()
